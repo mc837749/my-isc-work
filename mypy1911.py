1cbd054ba06f02d4945f3a32c367349e9c85c2e7
Jasmine George <jasmine.george@pgr.reading.ac.uk>
2019-11-19 15:12:52 +0000

    Python exerices

---
+++ mypy1911.py
@@ -0,0 +1,27 @@
+#EXERICE 1
+#Q1 - creating variables
+course = 'python'
+rating = 15
+print(course)
+print(rating)
+#Q2 - Use Pythagoras theorem (use **0.5 instead of sqrt function)
+b = 2
+c = 3
+
+a = (b*2 + c*2)**0.5
+print(a)
+
+#Q3 - date types
+print(type(a)) # a is different from b and c as it is not an integer
+print(type(b))
+print(type(c))
+
+#Q4 - convert between data types
+
+print(int(a))
+print(type(a))
+
+print(a + 'squared equals' + b + 'squared +' + c + ' squared.')
+
+
+

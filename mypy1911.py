1797d1bca1bbba87c9984d179cf0276a8464821c
Jasmine George <jasmine.george@pgr.reading.ac.uk>
2019-11-22 10:39:29 +0000

    Added exercises

--- mypy1911.py
+++ mypy1911.py
@@ -658,6 +658,33 @@ plt.show()
 
 #Q3
 
+from example_code.map_data import *
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure()
+m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=-180, urcrnrlon=180, resolution='c')
+m.drawcoastlines()
+
+iml = m.pcolormesh(lons,lats,tas, shading='flat', cmap=plt.cm.jet,latlon=True)
+
+plt.savefig('tas1.png')
+plt.show()
+
+fig.figure()
+plt.title('Change in Surface Air Temperature from MOHC HadGEM2-ES')
+m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=-180, urcrnrlon=180, resolution='c')
+
+m.drawcoastlines()
+m.drawparallels(np.arange(-90.,99.,30.))
+m.drawmeridians(np.arange(-180.,180.,60.))
+iml = m.pcolormesh(lons,lats,tas, shading='flat', cmap=plt.cm.jet,latlon=True)
+cb = m.colorbar(iml, 'bottom', size='5%', pad='2%')
+
+plt.savefig(tas2.png)
+plt.show()
+
 
 
 
